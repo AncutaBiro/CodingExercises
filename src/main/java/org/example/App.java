174196aaa272463f373a600debe1f78f14ebbdba
AncutaBiro <ancuta.biro@yahoo.com>
2020-07-08 17:02:02 +0300

    practicing - 2 ex from LeetCode

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,26 +7,10 @@ import java.util.Arrays;
  *
  */
 
-public class App 
-{
+public class App {
+
     public static void main( String[] args )
     {
-        int [] x = {1,1,1,1};
-        x = runningSum(x);
-        System.out.print(Arrays.toString(x));
 
-    }
-
-
-    public static int[] runningSum(int[] nums) {
-        int sum = 0;
-        int[] result = new int[nums.length];
-
-        for (int i = 0; i < nums.length; i++) {
-            sum = sum + nums[i];
-            result[i] = sum;
-        }
-
-        return result;
     }
 }
