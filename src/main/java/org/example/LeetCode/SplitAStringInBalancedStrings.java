8f19ed394147eed8e820eb7979acfa30f9b4c3d4
AncutaBiro <ancuta.biro@yahoo.com>
2020-08-25 22:17:51 +0300

    practicing - 1 ex from LeetCode

--- src/main/java/org/example/LeetCode/SplitAStringInBalancedStrings.java
+++ src/main/java/org/example/LeetCode/SplitAStringInBalancedStrings.java
@@ -1,3 +1,13 @@
+/*Balanced strings are those who have equal quantity of 'L' and 'R' characters.
+Given a balanced string s split it in the maximum amount of balanced strings.
+Return the maximum amount of splitted balanced strings.
+
+Example 1:
+Input: s = "RLRRLLRLRL"
+Output: 4
+Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring contains same number of 'L' and 'R'.
+* */
+
 package org.example.LeetCode;
 
 public class SplitAStringInBalancedStrings {
